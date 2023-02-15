c9ed6f46871b4d990002b0a7980786394a7aaf77
DimasGS <dimasgalih@refactory.id>
2023-02-15 20:37:35 +0700

    import smacx form

--- android/src/main/java/org/xrstudio/xmpp/flutter_xmpp/Connection/FlutterXmppConnection.java
+++ android/src/main/java/org/xrstudio/xmpp/flutter_xmpp/Connection/FlutterXmppConnection.java
@@ -31,7 +31,7 @@ import org.jivesoftware.smackx.muc.MultiUserChat;
 import org.jivesoftware.smackx.muc.MultiUserChatManager;
 import org.jivesoftware.smackx.receipts.DeliveryReceipt;
 import org.jivesoftware.smackx.receipts.DeliveryReceiptRequest;
-//import org.jivesoftware.smackx.xdata.Form;
+import org.jivesoftware.smackx.xdata.Form;
 import org.jivesoftware.smackx.xdata.form.Form;
 import org.jxmpp.jid.EntityBareJid;
 import org.jxmpp.jid.Jid;
